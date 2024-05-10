6272b4f835e8c2dd83d7a627815aa06528e55240
Tifa Lockhart <1534538667@qq.com>
2024-05-10 08:53:12 +0800

    ble可以连接进行双向通信

---
+++ example/android/app/src/main/kotlin/com/lib/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lib.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/screens/device_screen.dart
@@ -0,0 +1,318 @@
+import 'dart:async';
+import 'dart:io';
+// import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import '../widgets/service_tile.dart';
+import '../widgets/characteristic_tile.dart';
+import '../widgets/descriptor_tile.dart';
+import '../utils/snackbar.dart';
+import '../utils/extra.dart';
+// import '../utils/log_util.dart';
+import 'dart:developer';
+import 'package:loggy/loggy.dart';
+
+class DeviceScreen extends StatefulWidget {
+  final BluetoothDevice device;
+
+  const DeviceScreen({Key? key, required this.device}) : super(key: key);
+
+  @override
+  State<DeviceScreen> createState() => _DeviceScreenState();
+}
+
+class _DeviceScreenState extends State<DeviceScreen> {
+  int? _rssi;
+  int? _mtuSize;
+  BluetoothConnectionState _connectionState = BluetoothConnectionState.disconnected;
+  List<BluetoothService> _services = [];
+  bool _isDiscoveringServices = false;
+  bool _isConnecting = false;
+  bool _isDisconnecting = false;
+
+  late StreamSubscription<BluetoothConnectionState> _connectionStateSubscription;
+  late StreamSubscription<bool> _isConnectingSubscription;
+  late StreamSubscription<bool> _isDisconnectingSubscription;
+  late StreamSubscription<int> _mtuSubscription;
+  TextEditingController _controller = TextEditingController();
+  // final FocusNode _focusNode = FocusNode();
+  // final ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    Loggy.initLoggy();
+    // LogUtil.init(title: "来自LogUtil", isDebug: true, limitLength: 8000);
+    _connectionStateSubscription = widget.device.connectionState.listen((state) async {
+      _connectionState = state;
+      if (state == BluetoothConnectionState.connected) {
+        _services = []; // must rediscover services
+      }
+      if (state == BluetoothConnectionState.connected && _rssi == null) {
+        _rssi = await widget.device.readRssi();
+        print("RSSI: $_rssi");
+      }
+      if (mounted) {
+        setState(() {});
+      }
+      // sleep(Duration(seconds: 1));
+      await onDiscoverServicesPressed();
+      await onRequestMtuPressed();
+
+      // print("[info]_services =$_services");
+      logDebug("type = ${_services.runtimeType} ,${_services.length}");
+      _services.forEach((service) {
+        print("service.characteristics = ${service.characteristics}");
+        print("service.includedServices = ${service.includedServices}");
+        print("-----------------------" * 4);
+      });
+
+      var s = _services.map((s) {
+        return s.characteristics.map((c) {
+          // print(c);
+          print("++++++++++++++++++++" * 4);
+          return c;
+        });
+      }).toList();
+      // logDebug("[info]s =$s");
+      // print(object)
+      // await c.setNotifyValue(c.isNotifying == false);
+    });
+
+    _mtuSubscription = widget.device.mtu.listen((value) {
+      _mtuSize = value;
+      print("MTU: $value");
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    _isConnectingSubscription = widget.device.isConnecting.listen((value) {
+      _isConnecting = value;
+      print("Is Connecting: $value");
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    _isDisconnectingSubscription = widget.device.isDisconnecting.listen((value) {
+      _isDisconnecting = value;
+      print("Is Disconnecting: $value");
+      if (mounted) {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _connectionStateSubscription.cancel();
+    _mtuSubscription.cancel();
+    _isConnectingSubscription.cancel();
+    _isDisconnectingSubscription.cancel();
+    super.dispose();
+  }
+
+  bool get isConnected {
+    return _connectionState == BluetoothConnectionState.connected;
+  }
+
+  Future onConnectPressed() async {
+    try {
+      await widget.device.connectAndUpdateStream();
+      Snackbar.show(ABC.c, "Connect: Success", success: true);
+    } catch (e) {
+      if (e is FlutterBluePlusException && e.code == FbpErrorCode.connectionCanceled.index) {
+        // ignore connections canceled by the user
+      } else {
+        Snackbar.show(ABC.c, prettyException("Connect Error:", e), success: false);
+      }
+    }
+  }
+
+  Future onCancelPressed() async {
+    try {
+      await widget.device.disconnectAndUpdateStream(queue: false);
+      Snackbar.show(ABC.c, "Cancel: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Cancel Error:", e), success: false);
+    }
+  }
+
+  Future onDisconnectPressed() async {
+    try {
+      await widget.device.disconnectAndUpdateStream();
+      Snackbar.show(ABC.c, "Disconnect: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Disconnect Error:", e), success: false);
+    }
+  }
+
+  Future onDiscoverServicesPressed() async {
+    if (mounted) {
+      print("1111Discovering Services ${mounted}");
+      setState(() {
+        _isDiscoveringServices = true;
+      });
+    }
+    try {
+      _services = await widget.device.discoverServices();
+      // print("ser : $_services");
+      // LogUtil.v("我是一个日志");
+      Snackbar.show(ABC.c, "Discover Services: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Discover Services Error:", e), success: false);
+    }
+    if (mounted) {
+      print("2222Discovering Services ${mounted}");
+      setState(() {
+        _isDiscoveringServices = false;
+      });
+    }
+  }
+
+  Future onRequestMtuPressed() async {
+    try {
+      await widget.device.requestMtu(223, predelay: 0);
+      Snackbar.show(ABC.c, "Request Mtu: Success", success: true);
+    } catch (e) {
+      Snackbar.show(ABC.c, prettyException("Change Mtu Error:", e), success: false);
+    }
+  }
+
+  List<Widget> _buildServiceTiles(BuildContext context, BluetoothDevice d) {
+    return _services
+        .map(
+          (s) => ServiceTile(
+            service: s,
+            characteristicTiles: s.characteristics.map((c) => _buildCharacteristicTile(c)).toList(),
+          ),
+        )
+        .toList();
+  }
+
+  CharacteristicTile _buildCharacteristicTile(BluetoothCharacteristic c) {
+    return CharacteristicTile(
+      characteristic: c,
+      descriptorTiles: c.descriptors.map((d) => DescriptorTile(descriptor: d)).toList(),
+    );
+  }
+
+  Widget buildSpinner(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.black12,
+          color: Colors.black26,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRemoteId(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text('${widget.device.remoteId}'),
+    );
+  }
+
+  Widget buildRssiTile(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        isConnected ? const Icon(Icons.bluetooth_connected) : const Icon(Icons.bluetooth_disabled),
+        Text(((isConnected && _rssi != null) ? '${_rssi!} dBm' : ''), style: Theme.of(context).textTheme.bodySmall)
+      ],
+    );
+  }
+
+  Widget buildGetServices(BuildContext context) {
+    return IndexedStack(
+      index: (_isDiscoveringServices) ? 1 : 0,
+      children: <Widget>[
+        TextButton(
+          child: const Text("Get Services"),
+          onPressed: onDiscoverServicesPressed,
+        ),
+        const IconButton(
+          icon: SizedBox(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.grey),
+            ),
+            width: 18.0,
+            height: 18.0,
+          ),
+          onPressed: null,
+        )
+      ],
+    );
+  }
+
+  Widget buildMtuTile(BuildContext context) {
+    return ListTile(
+        title: const Text('MTU Size'),
+        subtitle: Text('$_mtuSize bytes'),
+        trailing: IconButton(
+          icon: const Icon(Icons.edit),
+          onPressed: onRequestMtuPressed,
+        ));
+  }
+
+  Widget buildConnectButton(BuildContext context) {
+    return Row(children: [
+      if (_isConnecting || _isDisconnecting) buildSpinner(context),
+      TextButton(
+          onPressed: _isConnecting ? onCancelPressed : (isConnected ? onDisconnectPressed : onConnectPressed),
+          child: Text(
+            _isConnecting ? "CANCEL" : (isConnected ? "DISCONNECT" : "CONNECT"),
+            style: Theme.of(context).primaryTextTheme.labelLarge?.copyWith(color: Colors.blue),
+          ))
+    ]);
+  }
+
+  void _TextFieldValueProcess() {
+    print("The text is ${_controller.text}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("widget.device.platformName = ${widget.device.platformName}");
+    return ScaffoldMessenger(
+      key: Snackbar.snackBarKeyC,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.device.platformName),
+          actions: [buildConnectButton(context)],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              buildRemoteId(context),
+              ListTile(
+                leading: buildRssiTile(context),
+                title: Text('Device is ${_connectionState.toString().split('.')[1]}.'),
+                trailing: buildGetServices(context),
+              ),
+              buildMtuTile(context),
+              ..._buildServiceTiles(context, widget.device),
+              Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(children: [
+                    TextField(
+                        maxLines: 5, // 设置为null或大于1的值表示支持多行输入
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          hintText: 'Enter your text here',
+                          border: OutlineInputBorder(),
+                        )),
+                    ElevatedButton(onPressed: _TextFieldValueProcess, child: Text('Submit')),
+                  ])),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
